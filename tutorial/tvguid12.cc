98b12f16af4e04ced6be7f0caf7b555f32d4dacf
Tomasz Kłoczko <kloczek@fedoraproject.org>
2018-06-01 22:26:03 +0100

    - fix compile errors in tvgid06 and all next ones
    tvguid06.cc: In function ‘void readFile(const char*)’:
    tvguid06.cc:62:57: error: no match for ‘operator!=’ (operand types are ‘std::basic_istream<char>::__istream_type’ {aka ‘std::basic_istream<char>’} and ‘int’)
                    fileToView.getline( buf, maxLineLength ) != 0 )
                    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~
    tvguid06.cc:62:57: note: candidate: ‘operator!=(int, int)’ <built-in>
    tvguid06.cc:62:57: note:   no known conversion for argument 1 from ‘std::basic_istream<char>::__istream_type’ {aka ‘std::basic_istream<char>’} to ‘int’
    
    just s/fileToView.getline( buf, maxLineLength ) != 0/fileToView.getline( buf, maxLineLength )/

--- tutorial/tvguid12.cc
+++ tutorial/tvguid12.cc
@@ -105,7 +105,7 @@ void readFile( const char *fileName )
         {
         char buf[maxLineLength];
         while( lineCount < maxLines &&
-               fileToView.getline( buf, maxLineLength ) != 0 )
+               fileToView.getline( buf, maxLineLength ) )
             {
             lines[lineCount] = newStr( buf );
             lineCount++;
