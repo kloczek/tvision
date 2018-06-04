847441e15682578d3fc457aecbc07ee9a93001ca
Tomasz Kłoczko <kloczek@fedoraproject.org>
2018-06-04 23:52:38 +0100

    - add code optimisation test script with some stats generated using this script

--- tests/README.md
+++ tests/README.md
@@ -1,6 +1,17 @@
 ## This contains only tools used on testing the code, and some log files with tests results and statistics used od development stage.
 
+- ```optimisation.log```
+
+  Statistics about text, bss and data section size tvision library and all demo programs generated using different optiimisation methods
+
+- ```optimisation.sh```
+
+  Script which generates optimisation.log
+
 - ```warnings.log```
-  current stattistis about compile warnings and raw log of all compaile warnings
+
+  Current stattistis about compile warnings and raw log of all compaile warnings
+
 - ```warnings.sh```
-  script which generates ```warnimgs.log```
+
+  Script which generates ```warnimgs.log```
---
+++ tests/optimisation.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+if [ -e ../Makefile ]; then
+	make -C .. distclean
+fi
+
+timestamp="$(date +%F-%T)"
+
+CXXFLAGS_O2="-O2"
+CXXFLAGS_O2_lto="-O2 -flto"
+CXXFLAGS_Os="-Os"
+CXXFLAGS_Os_lto="-Os -flto"
+CXXFLAGS_max_opt="-Os -flto -fno-exceptions -fno-rtti"
+
+LDFLAGS_O2="-Wl,--as-needed"
+LDFLAGS_O2_lto="-O2 -Wl,--as-needed -flto -fuse-linker-plugin"
+LDFLAGS_Os="-Wl,--as-needed"
+LDFLAGS_Os_lto="-Os -Wl,--as-needed -flto -fuse-linker-plugin"
+LDFLAGS_max_opt="-Os -Wl,--as-needed -flto -fuse-linker-plugin"
+
+all_targets="O2 O2_lto Os Os_lto max_opt"
+
+rm -rf $all_targets {demo,background,listbox,load,nomenus,splash,tvedit,tvguid*,tvlife,validator,tvhc,libtvision.so.0.0*}
+for i in $all_targets; do
+	echo $i
+	mkdir $i
+	cd $i
+	echo "configure $i"
+	eval LDFLAGS="$(echo \$LDFLAGS_${i})"
+	eval CXXFLAGS="$(echo \$CXXFLAGS_${i})"
+	LDFLAGS="$LDFLAGS" \
+	CXXFLAGS="$CXXFLAGS" \
+	AR="gcc-ar" RANLIB="gcc-ranlib" NM="gcc-nm" \
+	../../configure --quiet
+	echo "make $i"
+	make -j10 >/dev/null 2>&1
+	echo "------------------------------------------------------" >> ../optimisation.log
+	echo LDFLAGS="$LDFLAGS" >> ../roptimisation.log
+	echo CXXFLAGS="$CXXFLAGS" >> ../roptimisation.log
+	echo "------------------------------------------------------" >> ../optimisation.log
+	cd ..
+	cp $i/*/.libs/{demo,background,listbox,load,nomenus,splash,tvedit,tvguid*,tvlife,validator,tvhc,libtvision.so.0.0*} .
+	size {demo,background,listbox,load,nomenus,splash,tvedit,tvguid*,tvlife,validator,tvhc,libtvision.so.0.0*} >> optimisation.log
+	for i in {1..7}; do echo >> optimisation.log; done
+	rm -rf {demo,background,listbox,load,nomenus,splash,tvedit,tvguid*,tvlife,validator,tvhc,libtvision.so.0.0*}
+done
