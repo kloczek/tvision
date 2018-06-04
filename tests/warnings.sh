e0fd11060b25d0a15b8143c0a21a9eb2599b878a
Tomasz Kłoczko <kloczek@fedoraproject.org>
2018-06-04 23:25:00 +0100

    - add script to generate stats about number of comppile warnings   (I want to bobserve progress on renmoving those warnings before   I'll start removing them)

---
+++ tests/warnings.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -e ../Makefile ]; then
+	make -C .. distclean
+fi
+
+rm -rf warnings
+mkdir warnings
+cd warnings
+LDFLAGS="-Wl,--as-needed -flto -fuse-linker-plugin" \
+CXXFLAGS="-Wall" \
+AR="gcc-ar" RANLIB="gcc-ranlib" NM="gcc-nm" \
+../../configure --quiet
+echo make
+make >/dev/null 2> >(tee ../warnings.err >&2)
+cd ..
+grep -- -W warnings.err | sed 's/.*\[//; s/\]//' | sort | uniq -c | sort -nr > warnings.log
+echo >> warnings.log
+echo "----------------------------------------------------------------" >> warnings.log
+cat warnings.err >> warnings.log
+rm -rf warnings warnings.err
