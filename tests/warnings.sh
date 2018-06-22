ab606bf69a4a08aa0911dd2b389019cd7a1ef601
Tomasz Kłoczko <kloczek@fedoraproject.org>
2018-06-22 21:13:45 +0100

    Add more compiler switches to show even more compile time warnings.

--- tests/warnings.sh
+++ tests/warnings.sh
@@ -11,7 +11,7 @@ rm -rf warnings
 mkdir warnings
 cd warnings
 LDFLAGS="-Wl,--as-needed -flto -fuse-linker-plugin" \
-CXXFLAGS="-Wall -Wextra -fno-diagnostics-show-caret -Os -Wp,-D_FORTIFY_SOURCE=2" \
+CXXFLAGS="-Wall -Wextra -fno-diagnostics-show-caret -Os -Wp,-D_FORTIFY_SOURCE=2 -fdiagnostics-show-option -Wcomment -Wfloat-conversion -Wfloat-equal -Wformat -Wformat-security -Wformat-signedness -Wimplicit-fallthrough -Wignored-qualifiers -Wlogical-not-parentheses -Wmaybe-uninitialized -Wmissing-braces -Wmissing-declarations -Wmissing-field-initializers -Wmissing-format-attribute -Wno-long-long -Wno-unreachable-code -Wparentheses -Wpointer-arith -Wredundant-decls -Wreturn-type -Wsequence-point -Wshadow -Wsign-compare -Wswitch -Wswitch-default -Wtype-limits -Wundef -Wuninitialized -Wunreachable-code -Wunused-but-set-variable -Wunused-function -Wunused-label -Wunused-parameter -Wunused-result -Wunused-value -Wunused-variable -Wwrite-strings" \
 AR="gcc-ar" RANLIB="gcc-ranlib" NM="gcc-nm" \
 ../../configure --quiet --enable-maintainer-mode
 echo make
