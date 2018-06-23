a0f96b82fafacd5d552a28886cd2140f52b13d8e
Tomasz Kłoczko <kloczek@fedoraproject.org>
2018-06-23 13:52:22 +0100

    Add generate list of warnings using clag++ -Weverything.

--- tests/warnings.sh
+++ tests/warnings.sh
@@ -12,6 +12,14 @@ mkdir warnings
 cd warnings
 
 LDFLAGS="-Wl,--as-needed -flto -fuse-linker-plugin" \
+CXXFLAGS="-O -Weverything -fno-caret-diagnostics" \
+AR="gcc-ar" RANLIB="gcc-ranlib" NM="gcc-nm" \
+CXX=clang++ \
+../../configure --quiet --enable-maintainer-mode
+echo make
+make -O -j20 >/dev/null 2> >(tee ../warnings-clang.err >&2)
+
+LDFLAGS="-Wl,--as-needed -flto -fuse-linker-plugin" \
 CXXFLAGS="-Wall -Wextra -fno-diagnostics-show-caret -Os -Wp,-D_FORTIFY_SOURCE=2 -fdiagnostics-show-option -Wcomment -Wfloat-conversion -Wfloat-equal -Wformat -Wformat-security -Wformat-signedness -Wimplicit-fallthrough -Wignored-qualifiers -Wlogical-not-parentheses -Wmaybe-uninitialized -Wmissing-braces -Wmissing-declarations -Wmissing-field-initializers -Wmissing-format-attribute -Wno-long-long -Wno-unreachable-code -Wparentheses -Wpointer-arith -Wredundant-decls -Wreturn-type -Wsequence-point -Wshadow -Wsign-compare -Wswitch -Wswitch-default -Wtype-limits -Wundef -Wuninitialized -Wunreachable-code -Wunused-but-set-variable -Wunused-function -Wunused-label -Wunused-parameter -Wunused-result -Wunused-value -Wunused-variable -Wwrite-strings" \
 AR="gcc-ar" RANLIB="gcc-ranlib" NM="gcc-nm" \
 ../../configure --quiet --enable-maintainer-mode
@@ -24,6 +32,11 @@ grep -- -W warnings-gcc.err | sed 's/.*\[//; s/\]//' | sort | uniq -c | sort -nr
 echo >> warnings-gcc.log
 echo "----------------------------------------------------------------" >> warnings-gcc.log
 cat warnings-gcc.err >> warnings-gcc.log
+
+grep -- -W warnings-clang.err | sed 's/.*\[//; s/\]//' | sort | uniq -c | sort -nr > warnings-clang.log
+echo >> warnings-clang.log
+echo "----------------------------------------------------------------" >> warnings-clang.log
+cat warnings-clang.err >> warnings-clang.log
 
 rm -rf warnings-*.err
 
