ac96cab9ef5dc250f002902434b7c8da65a76e04
Tomasz Kłoczko <kloczek@fedoraproject.org>
2018-06-23 16:17:53 +0100

    Fixed almosst half of clang -Wreserved-id-macro warnings. Renamed defines: * _TV_VERSION to TV_VERSION * all __INC_<FOO.H> to TV_INC_<FOO.H> (those defines are used only internally)

--- tvision/tv.h
+++ tvision/tv.h
@@ -42,75 +42,75 @@ Inc., 100 Borland Way, Scotts Valley, CA 95066.
  * tv.h
  */
 
-#define _TV_VERSION 0x0200
+#define TV_VERSION 0x0200
 
 #define Uses_EventCodes
 #define Uses_ViewCommands
 #define Uses_ipstream
 #define Uses_opstream
-#define __INC_STDDLG_H
+#define TV_INC_STDDLG_H
 
 #if defined( Uses_TApplication )
 #define Uses_TProgram
 #define Uses_TScreen
 #define Uses_TEventQueue
-#define __INC_APP_H
+#define TV_INC_APP_H
 #endif
 
 #if defined( Uses_TProgram )
 #define Uses_TEvent
 #define Uses_TGroup
-#define __INC_APP_H
+#define TV_INC_APP_H
 #endif
 
 #if defined( Uses_TDeskTop )
 #define Uses_TGroup
-#define __INC_APP_H
+#define TV_INC_APP_H
 #endif
 
 #if defined( Uses_TBackground )
 #define Uses_TView
-#define __INC_APP_H
+#define TV_INC_APP_H
 #endif
 
 #if defined( Uses_TReplaceDialogRec )
-#define __INC_EDITORS_H
+#define TV_INC_EDITORS_H
 #endif
 
 #if defined( Uses_TFindDialogRec )
-#define __INC_EDITORS_H
+#define TV_INC_EDITORS_H
 #endif
 
 #if defined( Uses_TEditWindow )
 #define Uses_TWindow
-#define __INC_EDITORS_H
+#define TV_INC_EDITORS_H
 #endif
 
 #if defined( Uses_TFileEditor )
 #define Uses_TEditor
-#define __INC_EDITORS_H
+#define TV_INC_EDITORS_H
 #endif
 
 #if defined( Uses_TMemo )
 #define Uses_TEditor
-#define __INC_EDITORS_H
+#define TV_INC_EDITORS_H
 #endif
 
 #if defined( Uses_TEditor )
 #define Uses_TView
 #define Uses_TPoint
-#define __INC_EDITORS_H
+#define TV_INC_EDITORS_H
 #endif
 
 #if defined( Uses_TIndicator )
 #define Uses_TView
 #define Uses_TPoint
-#define __INC_EDITORS_H
+#define TV_INC_EDITORS_H
 #endif
 
 #if defined( Uses_TTerminal )
 #define Uses_TTextDevice
-#define __INC_TEXTVIEW_H
+#define TV_INC_TEXTVIEW_H
 #endif
 
 #if defined( Uses_TOutline )
@@ -120,117 +120,117 @@ Inc., 100 Borland Way, Scotts Valley, CA 95066.
 #if defined( Uses_TOutlineViewer )
 #define Uses_TScroller
 #define Uses_TScrollBar
-#define __INC_OUTLINE_H
+#define TV_INC_OUTLINE_H
 #endif
 
 #if defined( Uses_TTextDevice )
 #define Uses_TScroller
-#define __INC_TEXTVIEW_H
+#define TV_INC_TEXTVIEW_H
 #endif
 
 #if defined( Uses_TStatusLine )
 #define Uses_TView
-#define __INC_MENUS_H
+#define TV_INC_MENUS_H
 #endif
 
 #if defined( Uses_TStatusDef )
-#define __INC_MENUS_H
+#define TV_INC_MENUS_H
 #endif
 
 #if defined( Uses_TStatusItem )
-#define __INC_MENUS_H
+#define TV_INC_MENUS_H
 #endif
 
 #if defined( Uses_TMenuPopup )
 #define Uses_TMenuBox
-#define __INC_MENUS_H
+#define TV_INC_MENUS_H
 #endif
 
 #if defined( Uses_TMenuBox )
 #define Uses_TMenuView
-#define __INC_MENUS_H
+#define TV_INC_MENUS_H
 #endif
 
 #if defined( Uses_TMenuBar )
 #define Uses_TMenuView
-#define __INC_MENUS_H
+#define TV_INC_MENUS_H
 #endif
 
 #if defined( Uses_TMenuView )
 #define Uses_TView
-#define __INC_MENUS_H
+#define TV_INC_MENUS_H
 #endif
 
 #if defined( Uses_TMenu )
-#define __INC_MENUS_H
+#define TV_INC_MENUS_H
 #endif
 
 #if defined( Uses_TSubMenu )
 #define Uses_TMenuItem
-#define __INC_MENUS_H
+#define TV_INC_MENUS_H
 #endif
 
 #if defined( Uses_TMenuItem )
-#define __INC_MENUS_H
+#define TV_INC_MENUS_H
 #endif
 
 #if defined( Uses_TColorDialog )
 #define Uses_TColorGroup
 #define Uses_TDialog
-#define __INC_COLORSEL_H
+#define TV_INC_COLORSEL_H
 #endif
 
 #if defined( Uses_TColorItemList )
 #define Uses_TListViewer
-#define __INC_COLORSEL_H
+#define TV_INC_COLORSEL_H
 #endif
 
 #if defined( Uses_TColorGroupList )
 #define Uses_TListViewer
-#define __INC_COLORSEL_H
+#define TV_INC_COLORSEL_H
 #endif
 
 #if defined( Uses_TColorDisplay )
 #define Uses_TView
-#define __INC_COLORSEL_H
+#define TV_INC_COLORSEL_H
 #endif
 
 #if defined( Uses_TMonoSelector )
-#define __INC_COLORSEL_H
+#define TV_INC_COLORSEL_H
 #endif
 
 #if defined( Uses_TMonoSelector )
 #define Uses_TCluster
-#define __INC_COLORSEL_H
+#define TV_INC_COLORSEL_H
 #endif
 
 #if defined( Uses_TColorSelector )
 #define Uses_TView
-#define __INC_COLORSEL_H
+#define TV_INC_COLORSEL_H
 #endif
 
 #if defined( Uses_TColorGroup )
-#define __INC_COLORSEL_H
+#define TV_INC_COLORSEL_H
 #endif
 
 #if defined( Uses_TColorItem )
-#define __INC_COLORSEL_H
+#define TV_INC_COLORSEL_H
 #endif
 
 #if defined( Uses_TChDirDialog )
 #define Uses_TDialog
-#define __INC_STDDLG_H
+#define TV_INC_STDDLG_H
 #endif
 
 #if defined( Uses_TDirListBox )
 #define Uses_TListBox
-#define __INC_STDDLG_H
+#define TV_INC_STDDLG_H
 #endif
 
 #if defined( Uses_TDirCollection )
 #define Uses_TCollection
 #define Uses_TDirEntry
-#define __INC_STDDLG_H
+#define TV_INC_STDDLG_H
 #endif
 
 #if defined ( Uses_TRangeValidator )
@@ -257,160 +257,160 @@ Inc., 100 Borland Way, Scotts Valley, CA 95066.
 #if defined ( Uses_TValidator )
 #define Uses_TObject
 #define Uses_TStreamable
-#define __INC_VALIDATOR_H
+#define TV_INC_VALIDATOR_H
 #endif
 
 #if defined( Uses_TDirEntry )
-#define __INC_STDDLG_H
+#define TV_INC_STDDLG_H
 #endif
 
 #if defined( Uses_TFileDialog )
 #define Uses_TDialog
-#define __INC_STDDLG_H
+#define TV_INC_STDDLG_H
 #endif
 
 #if defined( Uses_TFileInfoPane )
 #define Uses_TView
 #define Uses_TSearchRec
-#define __INC_STDDLG_H
+#define TV_INC_STDDLG_H
 #endif
 
 #if defined( Uses_TFileList )
 #define Uses_TSortedListBox
 #define Uses_TFileCollection
 #define Uses_TSearchRec
-#define __INC_STDDLG_H
+#define TV_INC_STDDLG_H
 #endif
 
 #if defined( Uses_TSortedListBox )
 #define Uses_TListBox
-#define __INC_STDDLG_H
+#define TV_INC_STDDLG_H
 #endif
 
 #if defined( Uses_TFileCollection )
 #define Uses_TSortedCollection
 #define Uses_TSearchRec
-#define __INC_STDDLG_H
+#define TV_INC_STDDLG_H
 #endif
 
 #if defined( Uses_TFileInputLine )
 #define Uses_TInputLine
-#define __INC_STDDLG_H
+#define TV_INC_STDDLG_H
 #endif
 
 #if defined( Uses_TSearchRec )
-#define __INC_STDDLG_H
+#define TV_INC_STDDLG_H
 #endif
 
 #if defined( Uses_THistory )
 #define Uses_TView
-#define __INC_DIALOGS_H
+#define TV_INC_DIALOGS_H
 #endif
 
 #if defined( Uses_THistoryWindow )
 #define Uses_TWindow
-#define __INC_DIALOGS_H
+#define TV_INC_DIALOGS_H
 #endif
 
 #if defined( Uses_THistoryViewer )
 #define Uses_TListViewer
-#define __INC_DIALOGS_H
+#define TV_INC_DIALOGS_H
 #endif
 
 #if defined( Uses_TLabel )
 #define Uses_TStaticText
-#define __INC_DIALOGS_H
+#define TV_INC_DIALOGS_H
 #endif
 
 #if defined( Uses_TParamText )
 #define Uses_TStaticText
-#define __INC_DIALOGS_H
+#define TV_INC_DIALOGS_H
 #endif
 
 #if defined( Uses_TStaticText )
 #define Uses_TView
-#define __INC_DIALOGS_H
+#define TV_INC_DIALOGS_H
 #endif
 
 #if defined( Uses_TListBox )
 #define Uses_TListViewer
-#define __INC_DIALOGS_H
+#define TV_INC_DIALOGS_H
 #endif
 
 #if defined( Uses_TCheckBoxes )
 #define Uses_TCluster
-#define __INC_DIALOGS_H
+#define TV_INC_DIALOGS_H
 #endif
 
 #if defined( Uses_TMultiCheckBoxes )
 #define Uses_TCluster
-#define __INC_DIALOGS_H
+#define TV_INC_DIALOGS_H
 #endif
 
 #if defined( Uses_TRadioButtons )
 #define Uses_TCluster
-#define __INC_DIALOGS_H
+#define TV_INC_DIALOGS_H
 #endif
 
 #if defined( Uses_TCluster )
 #define Uses_TView
-#define __INC_DIALOGS_H
+#define TV_INC_DIALOGS_H
 #endif
 
 #if defined( Uses_TSItem )
-#define __INC_DIALOGS_H
+#define TV_INC_DIALOGS_H
 #endif
 
 #if defined( Uses_TButton )
 #define Uses_TView
-#define __INC_DIALOGS_H
+#define TV_INC_DIALOGS_H
 #endif
 
 #if defined( Uses_TInputLine )
 #define Uses_TView
 #define Uses_TValidator
-#define __INC_DIALOGS_H
+#define TV_INC_DIALOGS_H
 #endif
 
 #if defined( Uses_TDialog )
 #define Uses_TWindow
-#define __INC_DIALOGS_H
+#define TV_INC_DIALOGS_H
 #endif
 
 #if defined( Uses_TVMemMgr )
-#define __INC_BUFFERS_H
+#define TV_INC_BUFFERS_H
 #endif
 
 #if defined( Uses_TWindow )
 #define Uses_TGroup
-#define __INC_VIEWS_H
+#define TV_INC_VIEWS_H
 #endif
 
 #if defined( Uses_TGroup )
 #define Uses_TView
 #define Uses_TRect
-#define __INC_VIEWS_H
+#define TV_INC_VIEWS_H
 #endif
 
 #if defined( Uses_TListViewer )
 #define Uses_TView
-#define __INC_VIEWS_H
+#define TV_INC_VIEWS_H
 #endif
 
 #if defined( Uses_TScroller )
 #define Uses_TView
 #define Uses_TPoint
-#define __INC_VIEWS_H
+#define TV_INC_VIEWS_H
 #endif
 
 #if defined( Uses_TScrollBar )
 #define Uses_TView
-#define __INC_VIEWS_H
+#define TV_INC_VIEWS_H
 #endif
 
 #if defined( Uses_TFrame )
 #define Uses_TView
-#define __INC_VIEWS_H
+#define TV_INC_VIEWS_H
 #endif
 
 #if defined( Uses_TView )
@@ -420,281 +420,281 @@ Inc., 100 Borland Way, Scotts Valley, CA 95066.
 #define Uses_TPoint
 #define Uses_TDrawBuffer
 #define Uses_TPalette
-#define __INC_VIEWS_H
+#define TV_INC_VIEWS_H
 #endif
 
 #if defined( Uses_TPalette )
-#define __INC_VIEWS_H
+#define TV_INC_VIEWS_H
 #endif
 
 #if defined( Uses_TCommandSet )
-#define __INC_VIEWS_H
+#define TV_INC_VIEWS_H
 #endif
 
 #if defined( Uses_ViewCommands )
-#define __INC_VIEWS_H
+#define TV_INC_VIEWS_H
 #endif
 
 #if defined( Uses_TStrListMaker )
 #define Uses_TObject
 #define Uses_TStreamable
 #define Uses_TStrIndexRec
-#define __INC_RESOURCE_H
+#define TV_INC_RESOURCE_H
 #endif
 
 #if defined( Uses_TStringList )
 #define Uses_TObject
 #define Uses_TStreamable
-#define __INC_RESOURCE_H
+#define TV_INC_RESOURCE_H
 #endif
 
 #if defined( Uses_TStrIndexRec )
-#define __INC_RESOURCE_H
+#define TV_INC_RESOURCE_H
 #endif
 
 #if defined( Uses_TResourceFile )
 #define Uses_TObject
-#define __INC_RESOURCE_H
+#define TV_INC_RESOURCE_H
 #endif
 
 #if defined( Uses_TResourceItem )
-#define __INC_RESOURCE_H
+#define TV_INC_RESOURCE_H
 #endif
 
 #if defined( Uses_TResourceCollection )
 #define Uses_TStringCollection
-#define __INC_RESOURCE_H
+#define TV_INC_RESOURCE_H
 #endif
 
 #if defined( Uses_TStringCollection )
 #define Uses_TSortedCollection
-#define __INC_RESOURCE_H
+#define TV_INC_RESOURCE_H
 #endif
 
 #if defined( Uses_MsgBox )
-#define __INC_MSGBOX_H
+#define TV_INC_MSGBOX_H
 #endif
 
 #if defined( Uses_TSystemError )
-#define __INC_SYSTEM_H
+#define TV_INC_SYSTEM_H
 #endif
 
 #if defined( Uses_TScreen )
-#define __INC_SYSTEM_H
+#define TV_INC_SYSTEM_H
 #endif
 
 #if defined( Uses_TEventQueue )
 #define Uses_TEvent
-#define __INC_SYSTEM_H
+#define TV_INC_SYSTEM_H
 #endif
 
 #if defined( Uses_TEvent )
 #define Uses_TPoint
-#define __INC_SYSTEM_H
+#define TV_INC_SYSTEM_H
 #endif
 
 #if defined( Uses_THardwareInfo )
-#define __INC_HARDWARE_H
+#define TV_INC_HARDWARE_H
 #endif
 
 #if defined( Uses_EventCodes )
-#define __INC_SYSTEM_H
+#define TV_INC_SYSTEM_H
 #endif
 
 #if defined( Uses_TSortedCollection )
 #define Uses_TNSSortedCollection
 #define Uses_TCollection
-#define __INC_OBJECTS_H
+#define TV_INC_OBJECTS_H
 #endif
 
 #if defined( Uses_TCollection )
 #define Uses_TNSCollection
 #define Uses_TStreamable
-#define __INC_OBJECTS_H
+#define TV_INC_OBJECTS_H
 #endif
 
 #if defined( Uses_TRect )
 #define Uses_TPoint
 #define Uses_ipstream
 #define Uses_opstream
-#define __INC_OBJECTS_H
+#define TV_INC_OBJECTS_H
 #endif
 
 #if defined( Uses_TPoint )
-#define __INC_OBJECTS_H
+#define TV_INC_OBJECTS_H
 #endif
 
 #if defined( Uses_TDrawBuffer )
-#define __INC_DRAWBUF_H
+#define TV_INC_DRAWBUF_H
 #endif
 
 #if defined( Uses_fpstream )
 #define Uses_fpbase
 #define Uses_iopstream
-#define __INC_TOBJSTRM_H
+#define TV_INC_TOBJSTRM_H
 #endif
 
 #if defined( Uses_ofpstream )
 #define Uses_fpbase
 #define Uses_opstream
-#define __INC_TOBJSTRM_H
+#define TV_INC_TOBJSTRM_H
 #endif
 
 #if defined( Uses_ifpstream )
 #define Uses_fpbase
 #define Uses_ipstream
-#define __INC_TOBJSTRM_H
+#define TV_INC_TOBJSTRM_H
 #endif
 
 #if defined( Uses_fpbase )
 #define Uses_pstream
-#define __INC_TOBJSTRM_H
+#define TV_INC_TOBJSTRM_H
 #endif
 
 #if defined( Uses_iopstream )
 #define Uses_ipstream
 #define Uses_opstream
-#define __INC_TOBJSTRM_H
+#define TV_INC_TOBJSTRM_H
 #endif
 
 #if defined( Uses_opstream )
 #define Uses_pstream
 #define Uses_TPWrittenObjects
-#define __INC_TOBJSTRM_H
+#define TV_INC_TOBJSTRM_H
 #endif
 
 #if defined( Uses_ipstream )
 #define Uses_pstream
 #define Uses_TPReadObjects
-#define __INC_TOBJSTRM_H
+#define TV_INC_TOBJSTRM_H
 #endif
 
 #if defined( Uses_pstream )
-#define __INC_TOBJSTRM_H
+#define TV_INC_TOBJSTRM_H
 #endif
 
 #if defined( Uses_TPReadObjects )
 #define Uses_TNSCollection
-#define __INC_TOBJSTRM_H
+#define TV_INC_TOBJSTRM_H
 #endif
 
 #if defined( Uses_TPWrittenObjects )
 #define Uses_TNSSortedCollection
-#define __INC_TOBJSTRM_H
+#define TV_INC_TOBJSTRM_H
 #endif
 
 #if defined( Uses_TStreamableTypes )
 #define Uses_TNSSortedCollection
-#define __INC_TOBJSTRM_H
+#define TV_INC_TOBJSTRM_H
 #endif
 
 #if defined( Uses_TStreamableClass )
-#define __INC_TOBJSTRM_H
+#define TV_INC_TOBJSTRM_H
 #endif
 
 #if defined( Uses_TStreamable )
-#define __INC_TOBJSTRM_H
+#define TV_INC_TOBJSTRM_H
 #endif
 
 #if defined( Uses_TNSSortedCollection )
 #define Uses_TNSCollection
-#define __INC_TVOBJS_H
+#define TV_INC_TVOBJS_H
 #endif
 
 #if defined( Uses_TNSCollection )
 #define Uses_TObject
-#define __INC_TVOBJS_H
+#define TV_INC_TVOBJS_H
 #endif
 
 #if defined( Uses_TObject )
-#define __INC_TVOBJS_H
+#define TV_INC_TVOBJS_H
 #endif
 
 #if defined( Uses_TKeys )
-#define __INC_TKEYS_H
+#define TV_INC_TKEYS_H
 #endif
 
 #include <tvconfig.h>
 #include <ttypes.h>
 
-//#if defined( __INC_HARDWARE_H )
+//#if defined( TV_INC_HARDWARE_H )
 //#include <hardware.h>
 //#endif
 
-#if defined( __INC_TKEYS_H )
+#if defined( TV_INC_TKEYS_H )
 #include <tkeys.h>
 #endif
 
 #include <util.h>
 
-#if defined( __INC_TVOBJS_H )
+#if defined( TV_INC_TVOBJS_H )
 #include <tvobjs.h>
 #endif
 
-#if defined( __INC_TOBJSTRM_H )
+#if defined( TV_INC_TOBJSTRM_H )
 #include <tobjstrm.h>
 #endif
 
-#if defined( __INC_DRAWBUF_H )
+#if defined( TV_INC_DRAWBUF_H )
 #include <drawbuf.h>
 #endif
 
-#if defined( __INC_OBJECTS_H )
+#if defined( TV_INC_OBJECTS_H )
 #include <objects.h>
 #endif
 
-#if defined( __INC_SYSTEM_H )
+#if defined( TV_INC_SYSTEM_H )
 #include <system.h>
 #endif
 
-#if defined( __INC_MSGBOX_H )
+#if defined( TV_INC_MSGBOX_H )
 #include <msgbox.h>
 #endif
 
-#if defined( __INC_RESOURCE_H )
+#if defined( TV_INC_RESOURCE_H )
 #include <resource.h>
 #endif
 
-#if defined( __INC_VIEWS_H )
+#if defined( TV_INC_VIEWS_H )
 #include <views.h>
 #endif
 
-#if defined( __INC_BUFFERS_H )
+#if defined( TV_INC_BUFFERS_H )
 #include <buffers.h>
 #endif
 
-#if defined( __INC_DIALOGS_H )
+#if defined( TV_INC_DIALOGS_H )
 #include <dialogs.h>
 #endif
 
-#if defined( __INC_VALIDATOR_H )
+#if defined( TV_INC_VALIDATOR_H )
 #include <validate.h>
 #endif
 
-#if defined( __INC_STDDLG_H )
+#if defined( TV_INC_STDDLG_H )
 #include <stddlg.h>
 #endif
 
-#if defined( __INC_COLORSEL_H )
+#if defined( TV_INC_COLORSEL_H )
 #include <colorsel.h>
 #endif
 
-#if defined( __INC_MENUS_H )
+#if defined( TV_INC_MENUS_H )
 #include <menus.h>
 #endif
 
-#if defined( __INC_TEXTVIEW_H )
+#if defined( TV_INC_TEXTVIEW_H )
 #include <textview.h>
 #endif
 
-#if defined( __INC_EDITORS_H )
+#if defined( TV_INC_EDITORS_H )
 #include <editors.h>
 #endif
 
-#if defined( __INC_OUTLINE_H )
+#if defined( TV_INC_OUTLINE_H )
 #include <outline.h>
 #endif
 
-#if defined( __INC_APP_H )
+#if defined( TV_INC_APP_H )
 #include <app.h>
 #endif
