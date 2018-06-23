002b9b55516a222681623419f608c626ec7603e7
Tomasz Kłoczko <kloczek@fedoraproject.org>
2018-06-23 14:23:43 +0100

    Remove paralel build because it disturbs order of the warnings.

--- tests/warnings.sh
+++ tests/warnings.sh
@@ -17,14 +17,16 @@ AR="gcc-ar" RANLIB="gcc-ranlib" NM="gcc-nm" \
 CXX=clang++ \
 ../../configure --quiet --enable-maintainer-mode
 echo make
-make -O -j20 >/dev/null 2> >(tee ../warnings-clang.err >&2)
+make >/dev/null 2> >(tee ../warnings-clang.err >&2)
+
+make clean
 
 LDFLAGS="-Wl,--as-needed -flto -fuse-linker-plugin" \
 CXXFLAGS="-Wall -Wextra -fno-diagnostics-show-caret -Os -Wp,-D_FORTIFY_SOURCE=2 -fdiagnostics-show-option -Wcomment -Wfloat-conversion -Wfloat-equal -Wformat -Wformat-security -Wformat-signedness -Wimplicit-fallthrough -Wignored-qualifiers -Wlogical-not-parentheses -Wmaybe-uninitialized -Wmissing-braces -Wmissing-declarations -Wmissing-field-initializers -Wmissing-format-attribute -Wno-long-long -Wno-unreachable-code -Wparentheses -Wpointer-arith -Wredundant-decls -Wreturn-type -Wsequence-point -Wshadow -Wsign-compare -Wswitch -Wswitch-default -Wtype-limits -Wundef -Wuninitialized -Wunreachable-code -Wunused-but-set-variable -Wunused-function -Wunused-label -Wunused-parameter -Wunused-result -Wunused-value -Wunused-variable -Wwrite-strings" \
 AR="gcc-ar" RANLIB="gcc-ranlib" NM="gcc-nm" \
 ../../configure --quiet --enable-maintainer-mode
 echo make
-make -O -j20 >/dev/null 2> >(tee ../warnings-gcc.err >&2)
+make >/dev/null 2> >(tee ../warnings-gcc.err >&2)
 
 cd ..
 
